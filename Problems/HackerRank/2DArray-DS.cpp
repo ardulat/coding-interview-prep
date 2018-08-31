1a619855b8d554b953b2edd2eca809a31e3c30a3
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2018-08-31 14:58:06 +0600

    feat: started solving HackerRank practice probles

---
+++ Problems/HackerRank/2DArray-DS.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the hourglassSum function below.
+int hourglassSum(vector<vector<int>> arr) {
+    vector<int> hourglasses;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            int sum = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+            sum += arr[i+1][j+1];
+            sum += arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+            hourglasses.push_back(sum);
+        }
+    }
+    
+    for (int i = 0; i < 16; i++) {
+        cout << hourglasses[i] << ' ';
+    }
+    cout << endl;
+    
+    // maximum hourglass sum
+    return *max_element(hourglasses.begin(), hourglasses.end());
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> arr(6);
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = hourglassSum(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
