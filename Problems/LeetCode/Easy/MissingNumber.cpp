2d48f2c0188f61cf59b85e6af76611453f223a75
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2018-11-05 18:15:27 +0600

    feat: missing number solved

---
+++ Problems/LeetCode/Easy/MissingNumber.cpp
@@ -0,0 +1,35 @@
+// 
+
+// Approach 1: use math
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        // determine n and total sum
+        int n = nums.size();
+        int total = n*(n+1)/2;
+        
+        // subtract all numbers present
+        for (int num : nums) {
+            total = total-num;
+        }
+        
+        return total;
+    }
+};
+
+// Approach 2: use XOR
+// Note: revise binary operations!
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int xor_true = 1;
+        for (int i = 2; i <= n; i++)
+            xor_true = xor_true^i;
+        int xor_actual = nums[0];
+        for (int i = 1; i < n; i++)
+            xor_actual = xor_actual^nums[i];
+        
+        return xor_actual^xor_true;
+    }
+};
