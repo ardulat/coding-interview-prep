471806bee107332ae7860ddb48aad79a2c32588f
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2018-11-07 14:06:34 +0600

    fix: added approach description to the last solved

--- Problems/LeetCode/Easy/ExcelSheetColumnTitle.cpp
+++ Problems/LeetCode/Easy/ExcelSheetColumnTitle.cpp
@@ -1,5 +1,13 @@
 // https://leetcode.com/problems/excel-sheet-column-title/
 
+// Intuitive approach:
+// 1. if in range [1, 26], add 64 to it (convert to ASCII)
+// 2. if not, then: 
+//       - divie by 26, store it for later usage
+//       - get modulus of 26 to know, which character to put at the end of string
+// 3. Repeat until divided (and stored) number / division is not less than 26
+// 4. Reverse the string at the end
+
 class Solution {
 public:
     char to_ascii(int num) {
@@ -23,3 +31,18 @@ public:
         return res;
     }
 };
+
+// Interviewbit solution:
+string Solution::convertToTitle(int A) {
+    string result = "";
+    
+    while (A > 0) {
+        int x = (A-1) % 26;
+        result += char(65+x);
+        A = (A-1) / 26;
+    }
+    
+    reverse(result.begin(), result.end());
+    
+    return result;
+}
