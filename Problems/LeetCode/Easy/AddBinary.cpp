2042deeced1f1a4b2ab4fe10244a7e8510c97a2b
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2018-11-13 21:53:42 +0600

    fix: reduced the code complexity of add binary problem

--- Problems/LeetCode/Easy/AddBinary.cpp
+++ Problems/LeetCode/Easy/AddBinary.cpp
@@ -1,78 +1,39 @@
 // https://leetcode.com/problems/add-binary/
 
-// Note: too long code, reduce it
 class Solution {
 public:
+    int convert2int(char num) {
+        return num-'0';
+    }
+    
+    char convert2char(int num) {
+        return num+'0';
+    }
+    
     string addBinary(string a, string b) {
-        int n = a.size(), m = b.size();
-        int res_size = max(n, m);
-        string res (res_size, 'x');
+        string result = "";
+        int carry = 0;
+        int sum = 0;
         
-        int i = n-1, j = m-1, k = res_size-1;
-        int carry = 0; // number to carry to the prev
-        while (i >= 0 && j >= 0) {
-            int sum = a[i]-'0' + b[j]-'0' + carry;
-            if (sum > 1) {
-                carry = 1;
-                if (sum > 2)
-                    res[k] = '1';
-                else
-                    res[k] = '0';
-            }
-            else {
-                res[k] = sum+'0';
-                carry = 0;
-            }
+        int i = a.size()-1, j = b.size()-1;
+        cout << a[i] << ' '<< a[j] << endl;
+        
+        while (i >= 0 || j >= 0 || carry != 0) {
+            sum = 0;
+            if (i >= 0)
+                sum += convert2int(a[i]);
+            if (j >= 0)
+                sum += convert2int(b[j]);
+            sum += carry;
+            
+            result = convert2char(sum % 2) + result;
+            
+            carry = sum / 2;
+            
             i--;
             j--;
-            k--;
         }
         
-        while (i >= 0) {
-            int sum = a[i]-'0' + carry;
-            if (sum > 1) {
-                carry = 1;
-                if (sum > 2)
-                    res[k] = '1';
-                else
-                    res[k] = '0';
-            }
-            else {
-                res[k] = sum+'0';
-                carry = 0;
-            }
-            i--;
-            k--;
-        }
-        
-        while (j >= 0) {
-            int sum = b[j]-'0' + carry;
-            if (sum > 1) {
-                carry = 1;
-                if (sum > 2)
-                    res[k] = '1';
-                else
-                    res[k] = '0';
-            }
-            else {
-                res[k] = sum+'0';
-                carry = 0;
-            }
-            j--;
-            k--;
-        }
-        
-        if (carry == 1) {
-            string new_res (res_size+1, 'x');
-            new_res[0] = '1';
-            i = 0;
-            while (i < res_size) {
-                new_res[i+1] = res[i];
-                i++;
-            }
-            return new_res;
-        }
-        
-        return res;
+        return result;
     }
 };
