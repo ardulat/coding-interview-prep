e6791a4ce7d065b81d4933808c62f664919c79e2
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2018-11-13 18:31:03 +0600

    fix: implemented better version of finding peak element

--- Problems/LeetCode/Medium/FindPeakElement.cpp
+++ Problems/LeetCode/Medium/FindPeakElement.cpp
@@ -3,25 +3,20 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        
-        // better solution would be to extend our array
-        vector<int> newNums (nums.size()+2, 0);
-        newNums[0] = newNums[nums.size()+1] = INT_MIN;
-        
-        for (int i = 0; i < nums.size(); i++)
-            newNums[i+1] = nums[i];
-        
-        int start = 1, end = nums.size();
+        int n = nums.size();
+        int start = 0, end = n-1;
         
         while (start <= end) {
             int mid = (start + end) / 2;
             
-            if (newNums[mid] >= newNums[mid+1] && newNums[mid] >= newNums[mid-1])
-                return mid-1;
-            else if (newNums[mid-1] <= newNums[mid] && newNums[mid] <= newNums[mid+1])
-                start = mid+1;
-            else
+            if ((mid == 0 || nums[mid-1] <= nums[mid]) && 
+                (mid == n-1 || nums[mid+1] <= nums[mid]))
+                return mid;
+            
+            else if (mid > 0 && nums[mid-1] > nums[mid])
                 end = mid-1;
+            else
+                start = mid+1;
         }
         
         return -1;
