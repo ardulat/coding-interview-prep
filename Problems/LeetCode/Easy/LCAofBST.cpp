ee55d631fe6875e14b3994c68eb7f7f2eb5a06e5
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2018-11-14 14:20:29 +0600

    feat: lowest common ancestor in a binary search tree solved

---
+++ Problems/LeetCode/Easy/LCAofBST.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL)
+            return NULL;
+        
+        if (root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left, p, q);
+        if (root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right, p, q);
+        
+        return root;
+    }
+};
