70375873ef8293f9cadaede2cdd073c709dab2c2
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2018-11-15 16:38:32 +0600

    fix: modified the implementation for group anagrams problem

--- Problems/LeetCode/Medium/GroupAnagrams.cpp
+++ Problems/LeetCode/Medium/GroupAnagrams.cpp
@@ -6,19 +6,16 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> result;
-        unordered_map<string, vector<int>> hashmap;
+        unordered_map<string, vector<string>> hashmap;
         
         for (int i = 0; i < strs.size(); i++) {
             string s = strs[i]; // copy constructor
             sort(s.begin(), s.end());
-            hashmap[s].push_back(i);
+            hashmap[s].push_back(strs[i]);
         }
         
         for (auto& x : hashmap) {
-            vector<int> groupIndexes = x.second;
-            vector<string> groupStrings;
-            for (int index : groupIndexes)
-                groupStrings.push_back(strs[index]);
+            vector<string> groupStrings = x.second;
             result.push_back(groupStrings);
         }
         
