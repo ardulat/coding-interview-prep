8308687e65bfb215656b854378a76d78bbabb6f7
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2019-07-07 19:07:50 +0600

    feat: add new solution to next greater element in linked list

--- Problems/LeetCode/Medium/NextGreaterNodeInLinkedList.cpp
+++ Problems/LeetCode/Medium/NextGreaterNodeInLinkedList.cpp
@@ -26,3 +26,31 @@ public:
         return res;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int> res;
+        while (head != NULL) {
+            res.push_back(head->val);
+            head = head->next;
+        }
+        stack<int> st;
+        for (int i = res.size()-1; i >= 0; i--) {
+            int val = res[i];
+            while (!st.empty() && st.top() <= val)
+                st.pop();
+            res[i] = st.empty() ? 0 : st.top();
+            st.push(val);
+        }
+        return res;
+    }
+};
