0f05a05a533b517d9a1769a01b467f2d6c76562f
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2019-07-08 20:18:32 +0600

    feat: add new solution to reorder list

--- Problems/LeetCode/Medium/ReorderList.cpp
+++ Problems/LeetCode/Medium/ReorderList.cpp
@@ -18,10 +18,6 @@ public:
     Algorithm 2: O(N^2) time, O(1) space
     - count the size of array
     - start a counter and everytime traverse the list until reaching the node
-    
-    Algorithm 3: O(N) time, O(1) space
-    - reverse the right half of the list
-    - concatenate according to the order required
     */
     void reorderList(ListNode* head) {
         vector<ListNode*> nodes;
@@ -44,3 +40,51 @@ public:
 };
 
 
+// NOTE: The code is not that well-written
+class Solution {
+public:
+    /*
+    Algorithm 3: O(N) time, O(1) space
+    - reverse the right half of the list
+    - concatenate according to the order required using two pointers
+    */
+    ListNode* reverse(ListNode *head) {
+        ListNode *left = NULL, *right = head;
+        while (right != NULL) {
+            ListNode *temp = right->next; // NULL
+            right->next = left; // 4 -> 3
+            left = right; // left = 4
+            right = temp; // right = NULL
+        }
+        return left;
+    }
+    
+    void reorderList(ListNode* head) {
+        // handling base cases
+        if (head == NULL || head->next == NULL)
+            return;
+        // let's use the Floyd's algorithm to find the half
+        ListNode *slow = head, *fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        // separate both parts
+        ListNode *temp = head;
+        while (temp != NULL && temp->next != slow)
+            temp = temp->next;
+        if (temp != NULL)
+            temp->next = NULL;
+        ListNode *right = reverse(slow);
+        ListNode *left = head;
+        while (left != NULL && right != NULL) {
+            ListNode *tempLeft = left->next; // 3
+            ListNode *tempRight = right->next; // NULL
+            left->next = right; // 2 -> 3
+            if (tempLeft != NULL)
+                right->next = tempLeft; // 3 -> NULL
+            left = tempLeft; // 3
+            right = tempRight; // NULL
+        }
+    }
+};
