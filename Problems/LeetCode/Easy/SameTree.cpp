7fa9cf32a096b1abd6db9ecdbd0faf9ad5e5ad0e
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2019-07-15 14:23:43 +0600

    feat: same tree

---
+++ Problems/LeetCode/Easy/SameTree.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL)
+            return true;
+        if (p != NULL && q != NULL && p->val == q->val)
+            return isSameTree(p->left, q->left) & isSameTree(p->right, q->right);
+        return false;
+    }
+};
