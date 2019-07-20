dceaf4122173fcb5e7de063211add71ab4b1f87c
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2019-07-20 21:36:09 +0600

    feat: optimized the solution

--- Problems/LeetCode/Medium/SmallestIntegerDivisibleByK.cpp
+++ Problems/LeetCode/Medium/SmallestIntegerDivisibleByK.cpp
@@ -16,3 +16,24 @@ public:
         return count;
     }
 };
+
+// Optimized solution
+class Solution {
+public:
+    /*
+    K = 9
+    1 % 9 = 1
+    11 % 9 = 1*10+1 % 9 = 2
+    111 % 9 = 2*10+1 % 9 = 3
+    1111 % 9 = 3*10+1 % 9 = 4
+    */
+    int smallestRepunitDivByK(int K) {
+        if (K % 2 == 0 || K % 5 == 0) return -1;
+        int remainder = 0;
+        for (int i = 1; i <= K; i++) {
+            remainder = (remainder * 10 + 1) % K;
+            if (remainder == 0) return i;
+        }
+        return -1;
+    }
+};
