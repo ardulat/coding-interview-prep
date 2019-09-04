ac44e23e462f6c153fdce8b215ebad116048413b
Anuar <anuar.maratkhan@nu.edu.kz>
2019-09-04 13:46:22 +0600

    upd: pascals triangle

--- Problems/LeetCode/Easy/PascalsTriangle.cpp
+++ Problems/LeetCode/Easy/PascalsTriangle.cpp
@@ -4,31 +4,27 @@ class Solution {
 public:
     vector<int> generate(int numRows, vector<vector<int>>& res) {
         if (numRows < 3) {
-            vector<int> cur;
-            if (numRows == 0)
-                return cur;
-            cur.push_back(1);
-            res.push_back(cur);
+            vector<int> row;
+            row.push_back(1);
+            res.push_back(row);
             if (numRows == 1)
-                return cur;
-            cur.push_back(1);
-            res.push_back(cur);
-            return cur;
+                return row;
+            row.push_back(1);
+            res.push_back(row);
+            return row;
         }
-        vector<int> prev = generate(--numRows, res);
-        vector<int> cur;
-        cur.push_back(1); // left one
-        for (int i = 0; i < prev.size()-1; i++) {
-            int sum = prev[i] + prev[i+1];
-            cur.push_back(sum);
-        }
-        cur.push_back(1); // right one
+        vector<int> prev = res.size() == numRows-1 ? res[numRows-2] : generate(numRows-1, res);
+        vector<int> cur (prev.size()+1, 1); // row of size n+1 all filled with 1s
+        for (int i = 0; i < prev.size()-1; i++)
+            cur[i+1] = prev[i]+prev[i+1];
         res.push_back(cur);
         return cur;
     }
     
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res;
+        if (numRows == 0)
+            return res;
         generate(numRows, res);
         return res;
     }
