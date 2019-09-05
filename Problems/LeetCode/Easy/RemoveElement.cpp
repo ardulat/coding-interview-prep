1984fe3771c4285aa6e0a56a416297d352fd957c
Anuar <anuar.maratkhan@nu.edu.kz>
2019-09-05 18:35:44 +0600

    fix: update remove element

--- Problems/LeetCode/Easy/RemoveElement.cpp
+++ Problems/LeetCode/Easy/RemoveElement.cpp
@@ -21,3 +21,17 @@ public:
         return i;
     }
 };
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
