f03341ca0c8dd2ff480d0f502b0661d832e630ba
Anuar <anuar.maratkhan@nu.edu.kz>
2019-09-05 17:59:17 +0600

    fix: update reverse string

--- Problems/LeetCode/Easy/ReverseString.cpp
+++ Problems/LeetCode/Easy/ReverseString.cpp
@@ -12,3 +12,16 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0, j = s.size()-1;
+        
+        while (i < j) {
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+    }
+};
