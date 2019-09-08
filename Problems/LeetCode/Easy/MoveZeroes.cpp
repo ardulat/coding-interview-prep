c8eb0b8a9e7c100ed69049b4bcc094b1968637b1
Anuar <anuar.maratkhan@nu.edu.kz>
2019-09-08 15:08:21 +0600

    fix: update move zeroes

--- Problems/LeetCode/Easy/MoveZeroes.cpp
+++ Problems/LeetCode/Easy/MoveZeroes.cpp
@@ -12,3 +12,25 @@ public:
             }
     }
 };
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0 || n == 1)
+            return;
+        
+        int idx = 0;
+        
+        for (int i = 0; i < n; i++)
+            if (nums[i] != 0) {
+                swap(nums[i], nums[idx]);
+                idx++;
+            }
+        // fill the rest with zeroes
+        while (idx < n) {
+            nums[idx] = 0;
+            idx++;
+        }
+    }
+};
