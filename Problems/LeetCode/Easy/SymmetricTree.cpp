741faac67e0426775d1dd2bbb0163084901d67f8
Anuar <anuar.maratkhan@nu.edu.kz>
2019-09-13 17:15:34 +0600

    fix: update symmetric tree recursive

--- Problems/LeetCode/Easy/SymmetricTree.cpp
+++ Problems/LeetCode/Easy/SymmetricTree.cpp
@@ -51,3 +51,25 @@ public:
         return true;
     }
 };
+
+
+
+
+class Solution {
+public:
+    bool isSymmetric (TreeNode *left, TreeNode *right) {
+        if (left == NULL && right == NULL)
+            return true;
+        
+        if (left == NULL || right == NULL) // only one of them is NULL
+            return false;
+        
+        return left->val == right->val && 
+            isSymmetric (left->left, right->right) &&
+            isSymmetric (left->right, right->left);
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        return isSymmetric (root, root);
+    }
+};
