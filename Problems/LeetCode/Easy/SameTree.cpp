0e1fc7ae98774bcfc859282b62919eb88f79fd98
Anuar <anuar.maratkhan@nu.edu.kz>
2019-10-17 17:34:30 +0600

    fix: iterative approach for same tree added

--- Problems/LeetCode/Easy/SameTree.cpp
+++ Problems/LeetCode/Easy/SameTree.cpp
@@ -19,3 +19,45 @@ public:
         return false;
     }
 };
+
+// Iterative approach
+
+typedef pair<TreeNode*, TreeNode*> pr;
+
+class Solution {
+public:
+    bool check (TreeNode *first, TreeNode *second) {
+        if (first == NULL && second == NULL)
+            return true;
+        if (first == NULL || second == NULL)
+            return false;
+        
+        if (first->val != second->val)
+            return false;
+        
+        return true;
+    }
+    
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        queue<pr> nodes;
+        nodes.push({p, q});
+        
+        while (!nodes.empty()) {
+            pr p = nodes.front();
+            nodes.pop();
+            
+            TreeNode *first = p.first;
+            TreeNode *second = p.second;
+            
+            if (!check(first, second))
+                return false;
+            
+            if (first != NULL && second != NULL) {
+                nodes.push({first->left, second->left}); // lefts pair
+                nodes.push({first->right, second->right}); // rights pair
+            }
+        }
+        
+        return true;
+    }
+};
