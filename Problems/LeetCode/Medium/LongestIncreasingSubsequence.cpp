ba14198f88e96f6e73e3b5edfae79d68e804173e
Anuar <anuar.maratkhan@nu.edu.kz>
2019-10-28 15:14:02 +0600

    fix: dp iterative

--- Problems/LeetCode/Medium/LongestIncreasingSubsequence.cpp
+++ Problems/LeetCode/Medium/LongestIncreasingSubsequence.cpp
@@ -43,3 +43,34 @@ public:
         return lengthOfLIS(nums, INT_MIN, 0);
     }
 };
+
+// DP iterative
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        
+        if (n == 0)
+            return 0;
+        
+        int maxLen = 1, currentMax;
+        vector<int> dp(n, -1); // max len starting from index i
+        dp[0] = 1;
+        
+        for (int i = 1; i < n; i++) {
+            currentMax = 0;
+            
+            for (int j = i-1; j >= 0; j--) {
+                if (nums[i] > nums[j]) {
+                    currentMax = max(currentMax, dp[j]); // append to seq
+                }
+                // otherwise, do not append to seq
+            }
+            
+            dp[i] = currentMax+1; // prev seq + itself
+            maxLen = max(maxLen, dp[i]);
+        }
+        
+        return maxLen;
+    }
+};
